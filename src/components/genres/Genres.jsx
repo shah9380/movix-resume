69fcda85ca39cf879307059cd4b76a42c56b92e9
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-02-04 20:25:02 +0530

    added the details banner data and made changes for showing popup using video id and react player

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -5,16 +5,15 @@ import { useSelector } from 'react-redux'
 const Genres = ({data}) => {
 const {genres} = useSelector((state)=> state.home);
   return (
-    <div style={{zIndex: "20"}} className="genres">
+    <div className="genres">
         {data?.map((g)=>{
             if(!genres[g]?.name) return;
             return(
-                <div style={{zIndex: "20"}} key={g} className="genre">
+                <div key={g} className="genre">
                     {genres[g]?.name}
                 </div>
             )
         })}
-        <div>Hello</div>
     </div>
   )
 }
